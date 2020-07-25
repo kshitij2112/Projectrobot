89f68e6f79b4d70f0c71543d12642d76c4ba4929
Kshitij Prakhar <kshitijprakhar21@gmail.com>
2020-07-25 19:13:43 +0530

    Updates

---
+++ precache-manifest.f9ff29f33b2c4717f40e638e0d4e00e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6601b3a4201b963e23980b25adfb13",
+    "url": "/Projectrobot/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Projectrobot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "11a0867e8edc77d7514c",
+    "url": "/Projectrobot/static/css/main.839bba6b.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Projectrobot/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Projectrobot/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a0867e8edc77d7514c",
+    "url": "/Projectrobot/static/js/main.2c8ba0c2.chunk.js"
+  },
+  {
+    "revision": "031e0160a82a7064fe6b",
+    "url": "/Projectrobot/static/js/runtime-main.3b112960.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Projectrobot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
